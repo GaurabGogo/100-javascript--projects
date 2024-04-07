b866d11e3dcb59f556805ad282300f1aaf2f3f33
gaurabgogo <gaurabgogo@gmail.com>
2024-04-07 22:04:19 +0545

    1st project

---
+++ search-filter-sort/fakeclothes.js
@@ -0,0 +1,59 @@
+data = [
+  {
+    title:
+      "BULLMER Black Trendy Front and Back Printed Oversized Round Neck-Shirt for Men",
+    price: 1499,
+    image: "https://m.media-amazon.com/images/I/612Rl6GKHoL._AC_UL320_.jpg",
+    category: "topwear",
+  },
+  {
+    title: "Men T-Shirt || Regular Fit T-Shirt for Men || Plain T Shirt",
+    price: 349,
+    image: "https://m.media-amazon.com/images/I/71r3W2+d-vL._AC_UL320_.jpg",
+    category: "topwear",
+  },
+  {
+    title: "Shirt for Men | Tropical Leaf Printed Rayon Shirts for Men",
+    price: 289,
+    image: "https://m.media-amazon.com/images/I/61SmsCyfZOL._AC_UL320_.jpg",
+    category: "topwear",
+  },
+  {
+    title: "Pre & Post Pregnancy Pants for Women",
+    price: 999,
+    image: "https://m.media-amazon.com/images/I/61n-6NqbfgL._AC_UL320_.jpg",
+    category: "bottomwear",
+  },
+  {
+    title: "Dennis Lingo Men Slim Fit Cargo Trousers ",
+    price: 799,
+    image: "https://m.media-amazon.com/images/I/51sGZkbabiL._AC_UL320_.jpg",
+    category: "bottomwear",
+  },
+  {
+    title: "DMS Military Shoe Hand Mate Full Leather Black",
+    price: 899,
+    image: "https://m.media-amazon.com/images/I/21EzQ5JMmcL._AC_UL320_.jpg",
+    category: "footwear",
+  },
+  {
+    title: "unisex-adut AS002 White/Indigo Blue Slide Sandal",
+    price: 1199,
+    image: "https://m.media-amazon.com/images/I/5184YvefNML._AC_UL320_.jpg",
+    category: "footwear",
+  },
+  {
+    title: "Men's Watches Luxury Fashion Casual Dress Chronograph",
+    price: 2690,
+    image: "https://m.media-amazon.com/images/I/71tZYe9HVpL._AC_UL320_.jpg",
+    category: "accessories",
+  },
+  {
+    title: "Analog White Dial Women's Watch-FCN00037",
+    price: 1891,
+    image: "https://m.media-amazon.com/images/I/51fdT66lCuL._AC_UL320_.jpg",
+    category: "accessories",
+  },
+];
+
+export default data;
